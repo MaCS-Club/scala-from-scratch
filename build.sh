380784d7107a6f768f213535ad5dfee3168da95d
BeiZero <fedorov.igor95@gmail.com>
2018-09-25 21:18:59 +0400

    Fix theme

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-find ./lectures -iname "*.md"  -mindepth 2 -maxdepth 2 -type f -exec sh -c 'pandoc -t revealjs -s -o "./docs/$(basename ${0%.md}.html)" "${0}" -V revealjs-url=https://lab.hakim.se/reveal-js -V theme=league' {} \;
+find ./lectures -iname "*.md"  -mindepth 2 -maxdepth 2 -type f -exec sh -c 'pandoc -t revealjs -s -o "./docs/$(basename ${0%.md}.html)" "${0}" -V revealjs-url=https://macs-club.github.io/reveal.js -V theme=league' {} \;
